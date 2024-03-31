cbeffc4f495e384f695850d4556e7527410c7943
Steven Benner <admin@stevenbenner.com>
2024-03-30 22:42:22 -0700

    Create 2024 script

---
+++ 2024/rick.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+	const $img = $('<img/>');
+
+	$img[0].src = '/0401/2024/rick.jpg';
+	$img.css({
+		'display': 'none',
+		'position': 'absolute',
+		'top': '0',
+		'right': '0',
+		'min-width': '100%',
+		'min-height': '100%'
+	});
+
+	$img.on('load', () => {
+		$('body').append($img);
+		$img.fadeIn(300, () => {
+			$img.fadeOut(300, () => {
+				$img.remove();
+			})
+		});
+	});
+});
